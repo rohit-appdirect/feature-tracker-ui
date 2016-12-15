3e458940348548590406348ceb2404e5f484f153
Rohit Subhash Dhore <rohit.dhore@appdirect.com>
2016-12-15 12:57:31 +0530

    css changes and filter ui added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,28 @@
  */
 angular.module('featureTrackerUiApp')
   .controller('MainCtrl', function ($scope, dataService) {
+
+    //Column defination for grid
+    $scope.gridOptions = {
+        enableColumnResizing: true,
+        columnDefs: [
+            { field: 'jira_ticket'},
+            { field: 'feature'},
+            { field: 'pr_no' },
+            { field: 'screen_cast' },
+            { field: 'Ui_review_status'},
+            { field: 'team' },
+            { field: 'dev' },
+            { field: 'pm'},
+            { field: 'notes' },
+            { field: 'QA' },
+            { field: 'Release notes'},
+            { field: 'Blocker'}
+        ]
+    };
+
+    //Call data service to get the last updated data in database dump
     dataService.getPR().then(function(response) {
-        $scope.myData = response.data;
+        $scope.gridOptions = response.data;
     })
   });
